1b6e480d1d858e9559bf40b97fe91faf535d0236
Jimin Huang <jimin@chancefocus.com>
2023-10-20 10:57:11 -0400

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -8,8 +8,8 @@ TEST_FILENAME="Test Filename"
 
 # Call the Python script with the defined arguments
 python src/preprocess.py \
-  -dataset $DATASET \
-  -train_filename $TRAIN_FILENAME \
-  -valid_filename $VALID_FILENAME \
-  -test_filename $TEST_FILENAME \
-  -for_eval
+  --dataset $DATASET \
+  --train_filename $TRAIN_FILENAME \
+  --valid_filename $VALID_FILENAME \
+  --test_filename $TEST_FILENAME \
+  --for_eval
